c26584a0777be71d3da78912b468b62ed53880d1
ASAD18-AJ <123192655+ASAD18-AJ@users.noreply.github.com>
2024-09-22 23:48:07 +0530

    commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Hero from "../components/Home/Hero"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='bg-zinc-900 text-white px-10 py-8'>
+      <Hero />
+    </div>
   )
 }
 
-export default Home
+export default Home;
