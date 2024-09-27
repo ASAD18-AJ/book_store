dc4147302422a92174df3e02b74a55c475a2ede0
ASAD18-AJ <123192655+ASAD18-AJ@users.noreply.github.com>
2024-09-27 23:12:34 +0530

    commit

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='bg-zinc-800 text-white px-8 py-4'>
+    <div className='bg-zinc-800 text-white px-8 py-4 pt-8'>
       <h1 className='text-xl font-semibold text-center'>
         &copy; 2024, Made With Love By THE CODE MASTER
       </h1>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from "../components/Home/Hero"
+import RecentlyAdded from '../components/Home/RecentlyAdded';
 
 const Home = () => {
   return (
     <div className='bg-[#2E280E] text-white px-10 py-8'>
       <Hero />
+      <RecentlyAdded />
     </div>
   )
 }
