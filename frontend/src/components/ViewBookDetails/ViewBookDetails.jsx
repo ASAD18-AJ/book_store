3de99aad5e0ec83c4293d898af16213e5c205687
ASAD18-AJ <123192655+ASAD18-AJ@users.noreply.github.com>
2024-09-29 23:52:18 +0530

    commit

--- frontend/src/components/ViewBookDetails/ViewBookDetails.jsx
+++ frontend/src/components/ViewBookDetails/ViewBookDetails.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-// import Loader from '../Loader/Loader'
+import Loader from '../Loader/Loader'
 import { useParams } from 'react-router-dom'
+import { GrLanguage } from "react-icons/gr";
 
 const ViewBookDetails = () => {
     const { id } = useParams();
-    const [data, setData] = useState();
+    const [Data, setData] = useState('');
     useEffect(() => {
         const fetch = async () => {
           const response = await axios.get(
@@ -18,10 +19,30 @@ const ViewBookDetails = () => {
       }, []);
     
   return (
-    <div className='px-12 py-8 bg-zinc-900 flex gap-8'>
-        <div className="bg-zinc-800 rounded p-4 h-[88vh] w-3/6 flex items-center justify-center"> <img src={data.url} alt="" /></div>
-        <div className="p-4 w-3/6"></div>
-    </div>
+    <>
+    {Data && (
+      <div className='px-12 py-8 bg-zinc-900 flex gap-8'>
+      <div className="bg-zinc-800 rounded p-4 h-[88vh] w-3/6 flex items-center justify-center">
+       {" "}
+       <img src={Data.url}  className='h-[70vh]' alt="" />
+      </div>
+
+      <div className="p-4 w-3/6">
+        <h1 className="text-4xl text-zinc-300 font-semibold">{Data.title}</h1>
+        <p className="text-zinc-400 mt-1">by {Data.author}</p>
+        <p className="text-zinc-500 mt-4 text-xl">{Data.desc}</p>
+        <p className="flex mt-4 items-center justify-start text-zinc-400">
+          <GrLanguage className='me-3' /> {Data.language}
+        </p>
+        <p className="mt-4 text-zinc-100 text-3xl font-semibold">
+          Price : $ {Data.price}{" "}
+        </p>
+      </div>
+  </div>
+    )}
+    {!Data && <div className="h-screen bg-zinc-900 flex items-center justify-center"><Loader/></div> 
+    }
+    </>
   )
 }
 
