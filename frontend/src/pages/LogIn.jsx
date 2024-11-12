718df4062d428b88ef6c1ed61318bbb2fe4b6595
Asad Jahangir <123192655+ASAD18-AJ@users.noreply.github.com>
2024-11-12 17:07:50 +0530

    Update LogIn.jsx

--- frontend/src/pages/LogIn.jsx
+++ frontend/src/pages/LogIn.jsx
@@ -34,7 +34,7 @@ const LogIn = () => {
         alert("All fields are required");
       } else {
         const response = await axios.post(
-          'http://localhost:1000/api/v1/sign-in',
+          'https://book-store-ncld.onrender.com/api/v1/sign-in',
           Values
         )
         // console.log(response.data);
