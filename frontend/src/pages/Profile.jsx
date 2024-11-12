55f13f65f472dedc3cb50c9ad879befba218230e
Asad Jahangir <123192655+ASAD18-AJ@users.noreply.github.com>
2024-11-12 16:23:37 +0530

    Update Profile.jsx

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -16,7 +16,7 @@ const Profile = () => {
   useEffect(() => {
     const fetch = async () => {
       const response = await axios.get(
-        "http://localhost:1000/api/v1/get-user-information",
+        "https://book-store-ncld.onrender.com/api/v1/get-user-information",
         { headers }
       );
       setProfile(response.data);
