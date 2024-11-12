9e36409663ad694bfed1d0ecd36120b7baa208c1
Asad Jahangir <123192655+ASAD18-AJ@users.noreply.github.com>
2024-11-12 16:17:10 +0530

    Update BookCard.jsx

--- frontend/src/components/BookCard/BookCard.jsx
+++ frontend/src/components/BookCard/BookCard.jsx
@@ -13,7 +13,7 @@ const BookCard = ({ data, favourite }) => {
   
   const handleRemoveBook = async () => {
     const response = await axios.put(
-      "http://localhost:1000/api/v1/remove-book-from-favourite",
+      "https://book-store-ncld.onrender.com/api/v1/remove-book-from-favourite",
       {},
       { headers }
     )
